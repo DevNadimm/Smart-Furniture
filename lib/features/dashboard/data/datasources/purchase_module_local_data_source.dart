fab8563f950d0e279e13b61458eb457a57d046c3
DevNadimm <nadimchowdhury87@gmail.com>
2025-08-07 19:15:03 +0600

    update

---
+++ lib/features/dashboard/data/datasources/purchase_module_local_data_source.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:smart_furniture/features/dashboard/domain/entities/sales_module.dart';
+
+class PurchaseModuleLocalDataSource {
+  static List<SubModule> getPurchaseModule(BuildContext context) {
+    final strings = AppLocalizations.of(context)!;
+
+    return [
+      SubModule(
+        title: strings.purchaseRecordTitle,
+        subTitle: strings.purchaseRecordSubtitle,
+        iconPath: 'assets/images/modules/purchase.png',
+        onTap: () {},
+      ),
+      SubModule(
+        title: strings.purchaseReturnTitle,
+        subTitle: strings.purchaseReturnSubtitle,
+        iconPath: 'assets/images/modules/sales_return.png',
+        onTap: () {},
+      ),
+    ];
+  }
+}
---
+++ lib/features/dashboard/presentation/pages/purchase_module_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:smart_furniture/features/dashboard/data/datasources/purchase_module_local_data_source.dart';
+import 'package:smart_furniture/features/dashboard/presentation/widgets/sub_module_tile.dart';
+
+class PurchaseModulePage extends StatelessWidget {
+  static Route route() => MaterialPageRoute(builder: (context) => const PurchaseModulePage());
+
+  const PurchaseModulePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final purchaseModules = PurchaseModuleLocalDataSource.getPurchaseModule(context);
+    final strings = AppLocalizations.of(context)!;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(strings.purchase),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: ListView.separated(
+          itemCount: purchaseModules.length,
+          separatorBuilder: (_, __) => const SizedBox(height: 16),
+          itemBuilder: (context, index) {
+            final module = purchaseModules[index];
+            return SalesModuleTile(module: module);
+          },
+        ),
+      ),
+    );
+  }
+}
