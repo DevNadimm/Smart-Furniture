86d7b94deee1ef5546c531e3a9ba00536935bff8
DevNadimm <nadimchowdhury87@gmail.com>
2025-08-11 22:24:18 +0600

    damage list created

---
+++ lib/features/administration/presentation/pages/damage_list_page.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:intl/intl.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:smart_furniture/core/utils/widgets/app_notifier.dart';
+import 'package:smart_furniture/core/utils/widgets/filter_bar.dart';
+import 'package:smart_furniture/core/utils/widgets/empty_state_widget.dart';
+import 'package:smart_furniture/core/utils/widgets/loader.dart';
+import 'package:smart_furniture/core/utils/widgets/searchable_bottom_sheet.dart';
+import 'package:smart_furniture/features/administration/presentation/blocs/damage_list/damage_list_bloc.dart';
+import 'package:smart_furniture/features/administration/presentation/blocs/product_list/product_list_bloc.dart';
+
+class DamageListPage extends StatefulWidget {
+  static Route route() => MaterialPageRoute(builder: (context) => const DamageListPage());
+
+  const DamageListPage({super.key});
+
+  @override
+  State<DamageListPage> createState() => _DamageListPageState();
+}
+
+class _DamageListPageState extends State<DamageListPage> {
+  final TextEditingController _productNameController = TextEditingController();
+  final TextEditingController _productIdController = TextEditingController();
+
+  /// Map to lookup productId by productName
+  Map<String, String> _productNameToId = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchProducts();
+  }
+
+  @override
+  void dispose() {
+    _productNameController.dispose();
+    _productIdController.dispose();
+    super.dispose();
+  }
+
+  void _fetchData() {
+    context.read<DamageListBloc>().add(
+      LoadDamageListEvent(_productIdController.text),
+    );
+  }
+
+  void _fetchProducts() {
+    context.read<ProductListBloc>().add(LoadProductListEvent(''));
+  }
+
+  void _resetDamageList() {
+    context.read<DamageListBloc>().add(ResetDamageListEvent());
+  }
+
+  Future<void> _selectDate(BuildContext context, TextEditingController controller) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2025),
+      lastDate: DateTime.now(),
+    );
+    if (picked != null) {
+      controller.text = DateFormat('yyyy-MM-dd').format(picked);
+    }
+  }
+
+  void _selectProductPicker(List<String> items) {
+    showBarModalBottomSheet(
+      context: context,
+      isDismissible: true,
+      builder: (_) {
+        return SearchableBottomSheet(
+          items: items,
+          title: 'Select Product',
+          subtitle: 'Choose a product from the list',
+          searchHint: 'Search Product',
+          selectedItem: _productNameController.text,
+          onItemSelected: (String selectedName) {
+            _productNameController.text = selectedName;
+            _productIdController.text = _productNameToId[selectedName] ?? '';
+          },
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final strings = AppLocalizations.of(context)!;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(strings.damageListTitle),
+        leading: IconButton(
+          onPressed: () {
+            _resetDamageList();
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back),
+        ),
+      ),
+      body: Column(
+        children: [
+          BlocBuilder<ProductListBloc, ProductListState>(
+            builder: (context, state) {
+              if (state is ProductListLoaded) {
+                _productNameToId = {
+                  for (var p in state.productListModel.data!)
+                    p.productName ?? '': p.id?.toString() ?? ''
+                };
+                return FilterBar(
+                  onApplyFilter: _fetchData,
+                  onSelectDate: _selectDate,
+                  showFilterPicker: true,
+                  filterPickerController: _productNameController,
+                  onFilterPickerTap: () {
+                    _selectProductPicker(state.productListModel.data!.map((e) => e.productName ?? '').toList());
+                  },
+                  filterPickerLabel: 'Select Product',
+                );
+              } else if (state is ProductListLoading) {
+                return FilterBar(
+                  onApplyFilter: _fetchData,
+                  onSelectDate: _selectDate,
+                  showFilterPicker: true,
+                  filterPickerLabel: 'Select Product',
+                );
+              } else {
+                return const SizedBox.shrink();
+              }
+            },
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: BlocConsumer<DamageListBloc, DamageListState>(
+                  listener: (context, state) {
+                    if (state is DamageListError) {
+                      AppNotifier.showToast(state.message);
+                    }
+                  },
+                  builder: (context, state) {
+                    if (state is DamageListLoading) {
+                      return const Loader();
+                    }
+                    if (state is DamageListLoaded) {
+                      if (state.damageListModel.data?.isEmpty ?? false) {
+                        return const EmptyStateWidget(
+                          title: 'No Damage Records Found',
+                          message: 'We couldn’t find any damage records for the selected filters. Try adjusting your filters or selecting a different time period.',
+                        );
+                      } else {
+                        return ListView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: state.damageListModel.data?.length ?? 0,
+                          itemBuilder: (context, index) {
+                            final data = state.damageListModel.data?[index];
+                            return Text(data!.product?.productName ?? 'N/A');
+                          },
+                        );
+                      }
+                    }
+                    return const SizedBox.shrink();
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
