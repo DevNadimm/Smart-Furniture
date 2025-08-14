f26e267b354b1551e108163797a287460f5fd083
DevNadimm <nadimchowdhury87@gmail.com>
2025-08-14 20:19:19 +0600

    update

--- lib/features/dashboard/data/datasources/module_local_data_source.dart
+++ lib/features/dashboard/data/datasources/module_local_data_source.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:smart_furniture/core/constants/image_paths.dart';
 import 'package:smart_furniture/features/dashboard/domain/entities/module.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:smart_furniture/features/dashboard/presentation/pages/administration_page.dart';
+import 'package:smart_furniture/features/dashboard/presentation/pages/administration_module_page.dart';
 import 'package:smart_furniture/features/dashboard/presentation/pages/hr_module_page.dart';
 import 'package:smart_furniture/features/dashboard/presentation/pages/purchase_module_page.dart';
 import 'package:smart_furniture/features/dashboard/presentation/pages/sales_module_page.dart';
