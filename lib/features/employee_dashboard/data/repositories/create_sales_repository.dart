673d602b0c721a66c1d7bb8bcfb2ca61d703860e
DevNadimm <nadimchowdhury87@gmail.com>
2026-02-10 02:52:06 +0600

    feat: Add CreateSalesRepository

---
+++ lib/features/employee_dashboard/data/repositories/create_sales_repository.dart
@@ -0,0 +1,34 @@
+import 'package:http/http.dart' as http;
+import 'package:smart_furniture/core/constants/api_endpoints.dart';
+import 'package:smart_furniture/core/constants/error_messages.dart';
+import 'package:smart_furniture/core/services/app_preferences.dart';
+
+class CreateSalesRepository {
+  static Future<bool?> createSales(Map<String, dynamic> body) async {
+    final api = ApiEndpoints(shop: '');
+    final endpoint = api.createSales;
+
+    final uri = Uri.parse(endpoint);
+    print('URL: $uri');
+
+    try {
+      final token = await AppPreferences.getUserId();
+      final response = await http.post(
+        uri,
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+        body: body,
+      );
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        return true;
+      } else {
+        throw Exception(ErrorMessages.createSalesFailed);
+      }
+    } catch (e) {
+      throw Exception(ErrorMessages.createSalesFailed);
+    }
+  }
+}
